5d803c0acf35941b692993979d677dec81e296a1
Vadim <rootfig@gmail.com>
2022-02-19 09:39:44 +0300

    Задание "2.15. Нужно больше функций"

---
+++ js/main.js
@@ -0,0 +1,21 @@
+const maxCommentLength = 140;
+
+// Ссылка на функцию https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+function getRandomIntInclusive(min, max) {
+  if (min >= 0 && min < max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  } else {
+    return false;
+  }
+}
+
+
+function checkCommentLength(commentLength) {
+  if (maxCommentLength >= commentLength) {
+    return true;
+  } return false;
+}
+
