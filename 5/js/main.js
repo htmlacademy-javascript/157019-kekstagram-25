aeb281fe0bd13c2c5f36438d40698175cd376371
Keks <keks@htmlacademy.ru>
2022-03-10 13:03:49 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/main.js
@@ -0,0 +1,9 @@
+import './util.js';
+import './thumbnail-rendering.js';
+import  {photoDiscription} from './data.js';
+
+// eslint-disable-next-line no-console
+console.log(
+  photoDiscription()
+);
+
