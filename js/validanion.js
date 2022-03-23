3a46b775f06c1f337efe154e5f80e44b73d943e5
Vadim <rootfig@gmail.com>
2022-03-23 20:12:29 +0300

    fix

--- js/validanion.js
+++ js/validanion.js
@@ -1,10 +1,8 @@
-import {countingItems, countingDuplicate} from './util.js'
+import {countingItems, countingDuplicate} from './util.js';
 
 const imgUploadForm = document.querySelector('.img-upload__form');
 const textHashtags = document.querySelector('.text__hashtags');
-// const textInfo = document.querySelector('.text-info');
 const reSpace = /\b#[A-Za-zА-Яа-яЁё0-9]/;
-
 const reHashtagSymbols = /^#[A-Za-zА-Яа-яЁё0-9]{2,20}$/;
 
 const pristine = new Pristine(imgUploadForm, {
@@ -14,22 +12,19 @@ const pristine = new Pristine(imgUploadForm, {
 });
 
 imgUploadForm.addEventListener('submit', (evt) => {
+
   // const hashtagsCollection = textHashtags.value.toLowerCase().trim().split(/\s+/);
-
-  console.log(textHashtags.value.toLowerCase().trim().split(' '));
-
   const validateUniqueHashtags = () => countingDuplicate(textHashtags.value.toLowerCase().trim().split(' ')) === 0;
+  // console.log('Все хештеги должны быть разными ' + validateUniqueHashtags());
   const validateCountHashtags = () => countingItems(textHashtags.value.toLowerCase().trim().split(' ')) <= 5;
-  console.log(validateCountHashtags());
+  // console.log('Не более 5 хештегов ' + validateCountHashtags());
   const validateSymbolsHashtags = () => {
     for ( const item of textHashtags.value.toLowerCase().trim().split(' ')) {
       if (reHashtagSymbols.test(item)) {continue;}
       return false;
     } return true;
   };
-
-  console.log(validateSymbolsHashtags());
-
+  // console.log('После # используй буквы и цифры ' + validateSymbolsHashtags());
   const validateSpace = () => !reSpace.test(textHashtags.value);
   const value = textHashtags.value.trim();
   if (value === '') {
