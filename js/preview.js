57099c1eba11fd5511a3cf66b8b8d1cfbc20bdaf
Vadim <rootfig@gmail.com>
2022-04-15 18:18:20 +0300

    Доработка

--- js/preview.js
+++ js/preview.js
@@ -2,7 +2,6 @@ const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
 const fileUpload = document.querySelector('.img-upload__input');
 const preview = document.querySelector('.img-upload__preview img');
-const buttonUpload = document.querySelector('.img-upload__submit');
 
 fileUpload.addEventListener('change', () => {
   const file = fileUpload.files[0];
@@ -13,8 +12,5 @@ fileUpload.addEventListener('change', () => {
   if (matches) {
     preview.src = URL.createObjectURL(file);
   }
-  buttonUpload.addEventListener('click', () => {
-    preview.src = '';
-  });
 });
 
